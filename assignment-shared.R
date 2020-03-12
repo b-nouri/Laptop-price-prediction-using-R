965de87bf251cc24a90e7e9f03bc9ba9e2849d47
behnouri <61915687+behnouri@users.noreply.github.com>
2020-03-12 22:58:50 +0100

    Update assignment-shared.R

--- assignment-shared.R
+++ assignment-shared.R
@@ -1,26 +1,26 @@
 #---------Setup Libraries------------------------------------
-
 #install.packages("tidyverse")
 #install.packages("naniar")
 #install.packages("VIM")
 #install.packages("DMwR")
 
 #---------Load Libraries ------------------------------------
-
+library(plyr)
 library(tidyverse)
 library(naniar)
 library(VIM)
 library(DMwR)
 
 #--------Load Data-------------------------------------------
-
 train_df <- read.csv("https://raw.githubusercontent.com/behnouri/lprice-prediction/master/train.csv", na.strings = c("", "NA"))
-test_df <- read.csv("https://raw.githubusercontent.com/behnouri/lprice-prediction/master/train.csv", na.strings=c("NA",""))
+test_df <- read.csv("https://raw.githubusercontent.com/behnouri/lprice-prediction/master/test.csv", na.strings=c("NA",""))
 
 #--------Prepare Train Data---------------------------------
 head(train_df)
 sum(is.na(train_df))
 colnames(train_df)[12] <- "dkeyboard"
+colnames(train_df)[1] <- "id"
+
 
 vis_miss(train_df,cluster= TRUE)
 gg_miss_var(train_df)
@@ -33,17 +33,29 @@ gg_miss_var(clean2)
 gg_miss_case(clean2)
 
 clean2$screen_surface <- mapvalues(clean2$screen_surface,c("glossy", "matte"), c("Glossy", "Matte"))
-glimpse(clean2)
 
 aggr(x = clean2[,8:20])
 glimpse(clean2)
 clean3_knn <- knnImputation(clean2)
 aggr(x=clean3_knn)
 
-#-------------Prepare Test Data-----------------------------------
+clean3_knn %>%
+  summarise_if(is.factor,nlevels)
+
+
+#-------Split Train Data to train/test subsets (80/20 percent) ----------------------
+require(caTools)
+set.seed(741)
+sample = sample.split(clean3_knn$id,SplitRatio = 0.8)
+training_subset =subset(clean3_knn,sample ==TRUE)
+test_subset = subset(clean3_knn,sample ==FALSE)
+
+
+#-------Prepare Test Data-----------------------------------
 colnames(test_df)[12] <- "dkeyboard"
+colnames(test_df)[1] <- "id"
 glimpse(test_df)
 sum(is.na(test_df))
-aggr(x=test_df[,7:17])
+aggr(x=test_df[,6:20])
 clean_test <- test_df
 clean_test$screen_surface <- mapvalues(clean_test$screen_surface,c("glossy","matte"),c("Glossy","Matte"))
